9b133c5b579a23b80bf287898d760ad95ba31c1e
rootmighty <57359254+rootmighty@users.noreply.github.com>
2025-03-17 18:16:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,23 @@ import streamlit as st
 import pandas as pd
 import duckdb
 
+st.write("""
+# SQL SRS 
+Spaced Repetition System SQL practice
+""")
+my_options = ["Joins", "GroupBY","CTE","SubQueries" "Windows Function"]
+with st.sidebar:
+    option = st.selectbox("What would you like to review",
+                          my_options,
+                          index= None,
+                          placeholder = "Select a thematic to work on ...")
+if option in my_options:
+    data = {"a":[1, 2, 3], "b":[4, 5, 6]}
+    df = pd.DataFrame(data)
 
+    input_query = st.text_area("Veuillez saisir votre requête SQL:")
+    if st.button("Valider la requête"):
+        result = duckdb.query(query=input_query).df()
+        st.dataframe(result)
+else:
+    st.warning("Please select a correct theme !!")
