fcfd39c556a63deb5c21ab00458cef2ddc3a5b64
Mighty GAGLOZOU <mightygaglozou@Mightys-MacBook-Pro.local>
2025-03-17 18:04:02 +0100

    conflit erase

--- app.py
+++ app.py
@@ -8,16 +8,3 @@ st.write("""
 # SQL SRS 
 Spaced Repetition System SQL practice
 """)
-
-option = st.selectbox("What would you like to review",
-                      ["Joins", "GroupBY","CTE","SubQueries" "Windows Function"],
-                      index= None,
-                      placeholder = "Select a thematic to work on ...")
-if option:
-    data = {"a":[1, 2, 3], "b":[4, 5, 6]}
-    df = pd.DataFrame(data)
-
-    input_query = st.text_area("Veuillez saisir votre requête SQL:")
-    if st.button("Valider la requête"):
-        result = duckdb.query(query=input_query).df()
-        st.dataframe(result)
