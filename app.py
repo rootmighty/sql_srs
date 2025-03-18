f668ffc1b19a0745fa4a99d445ca334317fc826a
Mighty GAGLOZOU <mightygaglozou@Mightys-MacBook-Pro.local>
2025-03-18 12:27:18 +0100

    Ajout d'une database avec les exercice et un memory state

--- app.py
+++ app.py
@@ -1,75 +1,62 @@
 # pylint: disable=missing-module-docstring
-import io
 
 import duckdb
-import pandas as pd
 import streamlit as st
 
-CSV_1 = """
-beverage,price
-orange juice,1.99
-coffee,2.99
-tea,1.99
-"""
-
-beverages = pd.read_csv(io.StringIO(CSV_1))
-
-CSV_2 = """
-food_items,food_price
-cookies, 1.99
-pizza, 2.99
-pasta, 1.99
-"""
-
-food_items = pd.read_csv(io.StringIO(CSV_2))
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
 ANSWER_STR = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
 
-solution_df = duckdb.query(query=ANSWER_STR).df()
+#solution_df = duckdb.query(query=ANSWER_STR).df()
 
 input_query = st.text_area("Veuillez saisir votre requête SQL:")
 
-my_options = ["Joins", "GroupBY", "CTE", "SubQueries", "Windows Function"]
+my_options = ["CrossJoins", "GroupBy", "CTE", "SubQueries", "Window_Functions"]
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review",
         my_options,
         index=None,
         placeholder="Select a thematic to work on ...",
     )
-if option in my_options:
+    st.write(f"You selected: {theme}")
+    if theme:
+        exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+        st.write(exercise)
 
-    if st.button("Valider la requête"):
-        result = duckdb.query(query=input_query).df()
-        st.dataframe(result)
-
-        if len(result.columns) != len(solution_df.columns):
-            st.write("Error: the number of columns is not the same")
-
-        n_diff_lines = result.shape[0] - solution_df.shape[0]
-        if n_diff_lines != 0:
-            st.write(f"Result has {n_diff_lines} lines differents from the solution")
-
-        try:
-            result = result[solution_df.columns]
-            st.dataframe(result.compare(solution_df))
-        except KeyError as e:
-            st.write("Some columns are missing in the result")
-
-    tab1, tab2 = st.tabs(["Tables", "Solution"])
-
-    with tab1:
-        st.write("Table: beverages")
-        st.dataframe(beverages)
-        st.write("Table: food_items")
-        st.dataframe(food_items)
-        st.write("Expected:")
-        st.dataframe(solution_df)
-    with tab2:
-        st.write(ANSWER_STR)
-
-else:
-    st.warning("Please select a correct theme !!")
+#if option in my_options:
+#
+#    if st.button("Valider la requête"):
+#        result = duckdb.query(query=input_query).df()
+#        st.dataframe(result)
+#
+#        if len(result.columns) != len(solution_df.columns):
+#            st.write("Error: the number of columns is not the same")
+#
+#        n_diff_lines = result.shape[0] - solution_df.shape[0]
+#        if n_diff_lines != 0:
+#            st.write(f"Result has {n_diff_lines} lines differents from the solution")
+#
+#        try:
+#            result = result[solution_df.columns]
+#            st.dataframe(result.compare(solution_df))
+#        except KeyError as e:
+#            st.write("Some columns are missing in the result")
+#
+#    tab1, tab2 = st.tabs(["Tables", "Solution"])
+#
+#    with tab1:
+#        st.write("Table: beverages")
+#        st.dataframe(beverages)
+#        st.write("Table: food_items")
+#        st.dataframe(food_items)
+#        st.write("Expected:")
+#        st.dataframe(solution_df)
+#    with tab2:
+#        st.write(ANSWER_STR)
+#
+#else:
+#    st.warning("Please select a correct theme !!")
---
+++ init_db.py
@@ -0,0 +1,44 @@
+import io
+
+import pandas as pd
+import duckdb
+
+con = duckdb.connect(database = "data/exercises_sql_tables.duckdb", read_only = False)
+#-------------------------------------------------------------------
+#EXERCISES LIST
+#===================================================================
+
+data ={
+    "theme":["CrossJoins", "Window_Functions"],
+    "exercise_name": ["beverages_and_food","simple_window"],
+    "tables": [["beverages", "food_items"],"simple_window"],
+    "last_reviewed": ["1970-01-01","1970-01-01"]
+}
+
+memory_state_df = pd.DataFrame(data)
+con. execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
+
+#--------------------------------------------------------------------
+# CROSS JOIN EXERCISES
+#====================================================================
+
+CSV_1 = """
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+"""
+
+beverages = pd.read_csv(io.StringIO(CSV_1))
+con.execute("CREATE TABLE IF NOT EXISTS beverages AS SELECT * FROM beverages")
+
+
+CSV_2 = """
+food_items,food_price
+cookie juice,2.5
+chocolatine, 2
+muffin, 3
+"""
+
+food_items = pd.read_csv(io.StringIO(CSV_2))
+con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
