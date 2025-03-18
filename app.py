54ac7f9d02b9854b4f5dbae812d11ae9fb931ef4
Mighty GAGLOZOU <mightygaglozou@Mightys-MacBook-Pro.local>
2025-03-18 13:56:40 +0100

    Ajout de la fonctionnalité affichage de la solution

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import duckdb
 import streamlit as st
+import ast
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
@@ -10,9 +11,7 @@ SELECT * FROM beverages
 CROSS JOIN food_items
 """
 
-#solution_df = duckdb.query(query=ANSWER_STR).df()
-
-input_query = st.text_area("Veuillez saisir votre requête SQL:")
+# solution_df = duckdb.query(query=ANSWER_STR).df()
 
 my_options = ["CrossJoins", "GroupBy", "CTE", "SubQueries", "Window_Functions"]
 with st.sidebar:
@@ -24,39 +23,54 @@ with st.sidebar:
     )
     st.write(f"You selected: {theme}")
     if theme:
-        exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+        exercise = con.execute(
+            f"SELECT * FROM memory_state WHERE theme = '{theme}'"
+        ).df()
         st.write(exercise)
 
-#if option in my_options:
-#
-#    if st.button("Valider la requête"):
-#        result = duckdb.query(query=input_query).df()
-#        st.dataframe(result)
-#
-#        if len(result.columns) != len(solution_df.columns):
-#            st.write("Error: the number of columns is not the same")
-#
-#        n_diff_lines = result.shape[0] - solution_df.shape[0]
-#        if n_diff_lines != 0:
-#            st.write(f"Result has {n_diff_lines} lines differents from the solution")
-#
-#        try:
-#            result = result[solution_df.columns]
-#            st.dataframe(result.compare(solution_df))
-#        except KeyError as e:
-#            st.write("Some columns are missing in the result")
-#
-#    tab1, tab2 = st.tabs(["Tables", "Solution"])
-#
-#    with tab1:
-#        st.write("Table: beverages")
-#        st.dataframe(beverages)
-#        st.write("Table: food_items")
-#        st.dataframe(food_items)
-#        st.write("Expected:")
-#        st.dataframe(solution_df)
-#    with tab2:
-#        st.write(ANSWER_STR)
-#
-#else:
-#    st.warning("Please select a correct theme !!")
+if theme in my_options:
+    input_query = st.text_area("Veuillez saisir votre requête SQL:")
+    button_validation = st.button("Valider la requête")
+    #
+    #        if len(result.columns) != len(solution_df.columns):
+    #            st.write("Error: the number of columns is not the same")
+    #
+    #        n_diff_lines = result.shape[0] - solution_df.shape[0]
+    #        if n_diff_lines != 0:
+    #            st.write(f"Result has {n_diff_lines} lines differents from the solution")
+    #
+    #        try:
+    #            result = result[solution_df.columns]
+    #            st.dataframe(result.compare(solution_df))
+    #        except KeyError as e:
+    #            st.write("Some columns are missing in the result")
+    #
+    tab1, tab2 = st.tabs(["Tables", "Solution"])
+
+    with tab1:
+        #    exercise_tables = ast.literal_eval(exercise.loc[0, 'tables'])
+        exercise_tables = exercise.loc[0, "tables"]
+        st.write(f"Tables: {exercise_tables}")
+        for table in exercise_tables:
+            st.write(f"Table: {table}")
+            df_table = con.execute(f"SELECT * FROM {table}").df()
+            st.dataframe(df_table)
+        # Resultats
+        if button_validation:
+            st.write("Your Result:")
+            result = con.execute(input_query).df()
+            st.dataframe(result)
+    #        st.write("Table: food_items")
+    #        st.dataframe(food_items)
+    #        st.write("Expected:")
+    #        st.dataframe(solution_df)
+    with tab2:
+        exercise_name = exercise.loc[
+            0, "exercise_name"
+        ]  # Récupérer première ligne de la colonne qui nous intéresse
+        with open(f"answers/{exercise_name}.sql", "r") as f:
+            ANSWER_STR = f.read()
+            st.write(ANSWER_STR)
+
+else:
+    st.warning("Please select a correct theme !!")
