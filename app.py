b68a58cf3c706b3dbe06dfaa3185d9c0c775324b
Mighty GAGLOZOU <mightygaglozou@Mightys-MacBook-Pro.local>
2025-03-18 16:10:44 +0100

    Ajout du code de création de la database sur une branche pour Cloud

--- app.py
+++ app.py
@@ -3,7 +3,28 @@
 import duckdb
 import streamlit as st
 import ast
+import os
+import logging
 
+# Vérifions que la donnée existe
+if "data" not in os.listdir():
+    logging.error(os.listdir())
+    logging.error("No data folder found! Creating data folder ...")
+    os.mkdir("data")
+
+if "exercises_sql_tables.duckdb" not in os.listdir("data"):
+    logging.error("No duckdb database file found! Creating the database file ...")
+    exec(
+        open("init_db.py").read()
+    )  # C'est un truc de hackeur mais on le fait car subprocess ne marche pas bien avec streamlit
+    # subprocess.run(["python", "init_db.py"])
+    logging.info("Data initialized")
+
+
+st.title("SQL SRS Applications - Exercises")
+st.write(
+    "Welcome to the SQL SRS Applicatiosn - Exercises. Please select a thematic to work on."
+)
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
 my_options = ["CrossJoins", "GroupBy", "CTE", "SubQueries", "Window_Functions"]
@@ -16,29 +37,19 @@ with st.sidebar:
     )
     st.write(f"You selected: {theme}")
     if theme:
-        exercise = con.execute(
-            f"SELECT * FROM memory_state WHERE theme = '{theme}'"
-        ).df()
+        exercise = (
+            con.execute(
+                f"SELECT theme, exercise_name, tables, last_reviewed FROM memory_state WHERE theme = '{theme}' ORDER BY last_reviewed ASC"
+            )
+            .df()
+            .reset_index(drop=True)
+        )  # On pouvait faire df().sort_values("last_reviewed")
         st.write(exercise)
+
 
 if theme in my_options:
     input_query = st.text_area("Veuillez saisir votre requête SQL:")
     button_validation = st.button("Valider la requête")
-    #
-    #        if len(result.columns) != len(solution_df.columns):
-    #            st.write("Error: the number of columns is not the same")
-    #
-    #        n_diff_lines = result.shape[0] - solution_df.shape[0]
-    #        if n_diff_lines != 0:
-    #            st.write(f"Result has {n_diff_lines} lines differents from the solution")
-    #
-    #        try:
-    #            result = result[solution_df.columns]
-    #            st.dataframe(result.compare(solution_df))
-    #        except KeyError as e:
-    #            st.write("Some columns are missing in the result")
-    #
-    tab1, tab2 = st.tabs(["Tables", "Solution"])
 
     exercise_name = exercise.loc[
         0, "exercise_name"
@@ -46,6 +57,8 @@ if theme in my_options:
     with open(f"answers/{exercise_name}.sql", "r") as f:
         ANSWER_STR = f.read()
     solution_df = con.execute(ANSWER_STR).df()
+
+    tab1, tab2 = st.tabs(["Tables", "Solution"])
 
     with tab1:
         #    exercise_tables = ast.literal_eval(exercise.loc[0, 'tables'])
@@ -56,15 +69,31 @@ if theme in my_options:
             df_table = con.execute(f"SELECT * FROM {table}").df()
             st.dataframe(df_table)
     with tab2:
-        st.write(ANSWER_STR)
+        st.text(ANSWER_STR)
 
     # Resultats
     if button_validation:
         try:
-            #Votre Résultat
+            # Votre Résultat
             st.write("Your Result:")
             result = con.execute(input_query).df()
             st.dataframe(result)
+
+            if len(result.columns) != len(solution_df.columns):
+                st.write("Error: the number of columns is not the same")
+
+            n_diff_lines = result.shape[0] - solution_df.shape[0]
+            if n_diff_lines != 0:
+                st.write(
+                    f"Result has {n_diff_lines} lines differents from the solution"
+                )
+
+            try:
+                result = result[solution_df.columns]
+                st.dataframe(result.compare(solution_df))
+            except KeyError as e:
+                st.write("Some columns are missing in the result")
+
         except AttributeError as e:
             st.warning("Please enter a valid SQL query !!")
 
